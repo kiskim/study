d0593525185c2fcbae5b9fd6794d2b4e25ef09d7
kiskim <rlarltn807@naver.com>
2020-09-30 22:16:17 +0900

    20200930

--- j/programmers/level2/p08_prime/Solution.java
+++ j/programmers/level2/p08_prime/Solution.java
@@ -2,7 +2,9 @@ package programmers.level2.p08_prime;
 
 public class Solution {
     public int solution(String numbers) {
-		int answer = 0;
+        int answer = 0;
+        StringBuilder sb = new StringBuilder(numbers);
+        
         return answer;
     }
 }
