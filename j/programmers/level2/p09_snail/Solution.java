157e8b0df141c0d9b06c7c70118a01b98e85b6a0
kiskim <rlarltn807@naver.com>
2020-11-15 16:11:37 +0900

    20201115

--- j/programmers/level2/p09_snail/Solution.java
+++ j/programmers/level2/p09_snail/Solution.java
@@ -2,25 +2,39 @@ package programmers.level2.p09_snail;
 
 public class Solution {
     public int[] solution(int n) {
-		int[] answer = {};
+		int [][] arr = new int[n][n];
 		int max = n * (n + 1) / 2;
-		int [] snail = new int[max];
-		int start = 0;
-		int index = start + 1;
-		for(int i = 0; i < n; i++)
+		int[] answer = new int[max];
+		// n * n 배열에서 실제 채워질 곳을 -1로 초기화
+		for(int i = 0; i < n; i ++)
 		{
-			index += i;
-			snail[index - 1] = i + 1;
+			for (int j = 0; j<=i; j++)
+				arr[i][j] = -1;
 		}
-		for(int i = max - (n - 1); i < max; i++)
-			snail[i] = snail[i - 1] + 1;
-		int i = n;
-		index = max - 1;
-		while(index - i != start)
+		//세로, 가로, 값
+		int i = 0, j = 0, k = 1;
+		arr[i][j] = k;
+		while (k < max)
 		{
-			snail[index - i] = snail[index] + 1;
-			index -= i;
-			i = i - 1;
+			// 공통 조건 : 다음 인덱스가 0보다 작은 것은 앞서 설정했던 입력될 곳 포시를 위해 입력한 -1 값을 확인학 위함
+			// 삼각형 좌변 조건
+			// i + 1, 즉 세로 인덱스가 n 보다 작은 경우 순서대로 k를 중가시키면서 입력
+			while( i + 1 < n && k < max && arr[i + 1][j] < 0)
+				arr[++i][j] = ++k;
+			// 밑변 조건
+			// j + 1, 가로 인덱스가 n보다 작을 때
+			while (j + 1 < n && k < max && arr[i][j + 1] < 0)
+				arr[i][++j] = ++k;
+			// 우변 조건
+			// 좌상향으로 이동해야하기 때문에 i, j 모두 -1하며 인덱스 값을 확인
+			while (i - 1 > 0 && i - 1 > 0 && k < max && arr[i - 1][j - 1] < 0)
+				arr[--i][--j] = ++k;
+		}
+		k = 0;
+		for (i = 0; i < n; i++) {
+			for (j = 0; j <= i; j++) {
+				answer[k++] = arr[i][j];
+			}
 		}
         return answer;
 	}
--- j/programmers/level2/p23_song/Solution.java
+++ j/programmers/level2/p23_song/Solution.java
@@ -1,24 +1,17 @@
 package programmers.level2.p23_song;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 
 public class Solution {
-	public int get_sec(String start, String end) {
-		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-		long diff = 0;
-		try {
-			Date d1 = sdf.parse(start);
-			Date d2 = sdf.parse(end);
-			diff = (d1.getTime() - d2.getTime() / 60000);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		return (int)diff;
+	public int get_sec(String start, String end){
+		int res = 0;
+		String [] st = start.split(":");
+		String [] ed = end.split(":");
+		res = Math.abs(Integer.parseInt(ed[0]) - Integer.parseInt(st[0])) * 60
+			+ (Integer.parseInt(ed[1]) - Integer.parseInt(st[1]));
+		return res + 1;
 	}
 	public class Music{
 		int time;
@@ -42,21 +35,19 @@ public class Solution {
 		for (int i = 0; i < musicinfos.length; i++) {
 			song = musicinfos[i].split(",");
 			int time = get_sec(song[0], song[1]);
-			for(String mss : ms){
+			for(String mss : ms)
 				song[3] = song[3].replaceAll(mss, mss.toLowerCase().toCharArray()[0]+"");
-				sb = new StringBuilder(song[3]);
-				while (sb.length() < time)
-					sb.append(song[3]);
-				song[3] = sb.substring(0, time);
-			}
-			if (song[3].contains(m))
+			sb = new StringBuilder(song[3]);
+			while (sb.length() < time)
+				sb.append(song[3]);
+			song[3] = sb.substring(0, time);
+			if (song[3].indexOf(m) > -1)
 				playlist.add(new Music(time, song[2], i));
 		}
 		if (playlist.size() == 0)
-			return "NONE";
+			return "(None)";
 		else if(playlist.size() > 1)
 			Collections.sort(playlist, new Comparator<Music>(){
-
 				@Override
 				public int compare(Music o1, Music o2) {
 					if (o1.time == o2.time)
@@ -68,10 +59,9 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-		String melody = "ABC#DEFG";
-		String [] music ={ "23:50,00:14,HELLO,C#DEFGAB", "13:00,13:15,WORLD,C#DEFGAB"};
+		String melody = "AA#";
+		String [] music ={  "13:00,13:01,HAPPY,BA#" };//, "00:00,00:14,HELLO,C#D#EFGAB","13:03,13:18,WORLD,C#DEFGAB"};
 		Solution s = new Solution();
 		System.out.println(s.solution(melody, music));
-		
 	}
 }
