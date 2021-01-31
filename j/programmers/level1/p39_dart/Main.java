4f49bd98a1b88bf7e865b581b9211543682551f9
kiskim <rlarltn807@naver.com>
2021-01-31 20:28:45 +0900

    20210131

--- j/programmers/level1/p39_dart/Main.java
+++ j/programmers/level1/p39_dart/Main.java
@@ -2,88 +2,43 @@ package programmers.level1.p39_dart;
 
 public class Main {
     public int solution(String dartResult) {
-		int temp = 0;
-		boolean ten = false;
-		int []point = new int[3];
-		int index = -1;
-		for(char c: dartResult.toCharArray())
-		{
-			if(c >= '0' && c <= '9')
-			{
-				if(ten == false){
-					if(index > -1 && index < 3){
-						point[index] = temp;
-					}
-					index++;
-					if(c == '1')
-						ten = true;
-					temp = c - '0';
-				}
-				else if(c == '0' && ten == true){
-					temp = 10;
-					ten = false;
-				}
-			}
-			else if(temp != 0)
-			{
-				ten = false;
-				switch(c)
-				{
-					case 'D': temp = temp * temp; break;
-					case 'T': temp = temp * temp * temp; break;
-					case '*': 
-						if(index > 0)
-							point[index-1] *=2;
-						temp *=2;
-						break;
-					case '#': temp = temp *(-1); break;
-				}
-			}
-		}
-		point[index]= temp;
-        return point[0] + point[1] + point[2];
-	}
-    public int solution2(String dartResult) {
 		int answer = 0;
-		int [] point = new int[3];
-		int pointCnt = 0;
-		for(int i = 0; i < dartResult.length(); i++)
-		{
-			char c = dartResult.charAt(i);
-			if(c >= '0' && c <= '9')
-			{
-				if(c == '1' && dartResult.charAt(i+1) == '0'){
-					point[pointCnt] = 10;
+		int idx = 0;
+		int [] score = new int[3];
+		for(int i = 0; i < dartResult.length(); i++){
+			if(dartResult.charAt(i) >= '0' && dartResult.charAt(i) <= '9'){
+				if(dartResult.charAt(i) == '1' && dartResult.charAt(i + 1) == '0'){
 					i++;
+					score[idx] = 10;
 				}
 				else
-					point[pointCnt] = c - '0';
+					score[idx] = dartResult.charAt(i) - '0';
 			}
 			else{
-				switch(c)
-				{
-					case 'S': point[pointCnt] = (int)Math.pow(point[pointCnt], 1); pointCnt++;break;
-					case 'D': point[pointCnt] = (int)Math.pow(point[pointCnt], 2); pointCnt++;break;
-					case 'T': point[pointCnt] = (int)Math.pow(point[pointCnt], 3); pointCnt++;break;
-					case '*':
-						point[pointCnt - 1] *= 2;
-						if(pointCnt - 2 >= 0)
-							point[pointCnt - 2] *= 2;
-						break;
-					case '#':
-						point[pointCnt - 1] *= -1;
-						break;
+				switch(dartResult.charAt(i)){
+					case 'S' : idx++; break;
+					case 'D' : 
+						score[idx] = score[idx] * score[idx];
+						idx++;	break;
+					case 'T' : 
+						score[idx] = score[idx] * score[idx] * score[idx];
+						idx++;	break;
+					case '*' :
+						score[idx - 1] *= 2; 
+						if (idx - 2 >= 0)
+							score[idx - 2] *=2;
+							break;
+					case '#' : score[idx - 1] *= -1; break;
 				}
 			}
 		}
-		for(int a: point)
-			answer+= a;
+		for(int s : score)
+			answer += s;
 		return answer;
 	}
-
 	public static void main(String[] args) {
 		String  s = "1D2S3T*";
 		Main m = new Main();
-		System.out.println(m.solution2(s)+"");
+		System.out.println(m.solution(s)+"");
 	}
 }
